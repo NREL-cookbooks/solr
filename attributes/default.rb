c292603c5f94dd5f122d02b890f2e2298f1f369c
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-06-29 20:21:55 +0000

    A more complete solr installation, with optional jar dependencies included from the tar file. Customize the schema and solr configuration some to suit our needs.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,4 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:solr][:home] = "/opt/solr"
+default[:solr][:root] = "/opt/solr"
+
+default[:solr][:version] = "3.2.0"
+default[:solr][:archive_checksum] = "4852e866f77ba2e4dc1639610316a64d384531d00d7e8963e0984ad11e6d2b5a"
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,20 +7,30 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe "rsync"
 include_recipe "tomcat"
 
-cookbook_file "#{node[:tomcat][:webapp_dir]}/solr.war" do
-  source "apache-solr-3.2.0.war"
-  owner node[:tomcat][:user]
-  group node[:tomcat][:group]
-  mode "0644"
-  notifies :restart, "service[tomcat6]"
-end
+node.set[:solr][:contrib_dir] = "#{node[:solr][:root]}/contrib"
+node.set[:solr][:dist_dir] = "#{node[:solr][:root]}/dist"
+node.set[:solr][:webapp_dir] = "#{node[:solr][:root]}/webapp"
 
-node.set[:solr][:conf_dir] = "#{node[:solr][:home]}/conf"
-node.set[:solr][:data_dir] = "#{node[:solr][:home]}/data"
+node.set[:solr][:instance][:home] = "#{node[:solr][:root]}/instance"
+node.set[:solr][:instance][:conf_dir] = "#{node[:solr][:instance][:home]}/conf"
+node.set[:solr][:instance][:data_dir] = "#{node[:solr][:instance][:home]}/data"
+node.set[:solr][:instance][:lib_dir] = "#{node[:solr][:instance][:home]}/lib"
 
-[node[:solr][:home], node[:solr][:conf_dir], node[:solr][:data_dir]].each do |path|
+create_dirs = [
+  node.set[:solr][:contrib_dir],
+  node.set[:solr][:dist_dir],
+  node.set[:solr][:webapp_dir],
+
+  node.set[:solr][:instance][:home],
+  node.set[:solr][:instance][:conf_dir],
+  node.set[:solr][:instance][:data_dir],
+  node.set[:solr][:instance][:lib_dir],
+]
+
+create_dirs.each do |path|
   directory path do
     owner node[:tomcat][:user]
     group node[:tomcat][:group]
@@ -29,7 +39,38 @@ node.set[:solr][:data_dir] = "#{node[:solr][:home]}/data"
   end
 end
 
-template "#{node[:solr][:home]}/solr.xml" do
+template "#{node[:tomcat][:context_dir]}/solr.xml" do
+  source "tomcat_context.xml.erb"
+  owner node[:tomcat][:user]
+  group node[:tomcat][:group]
+  mode "0644"
+  notifies :restart, "service[tomcat6]"
+end
+
+version = node[:solr][:version]
+
+remote_file "#{Chef::Config[:file_cache_path]}/apache-solr-#{version}.tgz" do
+  source "http://apache.cs.utah.edu/lucene/solr/#{version}/apache-solr-#{version}.tgz"
+  checksum node[:solr][:archive_checksum]
+end
+
+bash "install_solr" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    tar -xf apache-solr-#{version}.tgz
+
+    rsync -a --delete-delay apache-solr-#{version}/dist/ #{node[:solr][:dist_dir]}
+    rsync -a --delete-delay apache-solr-#{version}/contrib/ #{node[:solr][:contrib_dir]}
+    cp apache-solr-#{version}/dist/apache-solr-#{version}.war #{node[:solr][:webapp_dir]}/
+
+  EOH
+    #rm -rf #{tmp}/apache-solr-#{version}
+
+  not_if { ::File.exists?("#{node[:solr][:webapp_dir]}/apache-solr-#{version}.war") }
+  notifies :restart, "service[tomcat6]"
+end
+
+template "#{node[:solr][:instance][:home]}/solr.xml" do
   source "solr.xml.erb"
   owner node[:tomcat][:user]
   group node[:tomcat][:group]
@@ -38,7 +79,7 @@ template "#{node[:solr][:home]}/solr.xml" do
 end
 
 %w{elevate.xml mapping-FoldToASCII.txt mapping-ISOLatin1Accent.txt protwords.txt schema.xml scripts.conf solrconfig.xml spellings.txt stopwords.txt synonyms.txt}.each do |file|
-  template "#{node[:solr][:conf_dir]}/#{file}" do
+  template "#{node[:solr][:instance][:conf_dir]}/#{file}" do
     source "#{file}.erb"
     owner node[:tomcat][:user]
     group node[:tomcat][:group]
