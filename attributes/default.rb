766da1e0456b186a5b210f21b0da657ba37a9904
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-07-05 15:35:50 +0000

    Enable solr clustering feature.

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,3 +11,5 @@ default[:solr][:root] = "/opt/solr"
 
 default[:solr][:version] = "3.2.0"
 default[:solr][:archive_checksum] = "4852e866f77ba2e4dc1639610316a64d384531d00d7e8963e0984ad11e6d2b5a"
+
+default[:solr][:clustering][:enabled] = true
