63da98c0be489624cefd4657448dfa13ec575fd0
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-09-06 18:38:49 +0000

    Add setup for enabling a separate test core in solr, for use in unit testing.

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,4 +12,6 @@ default[:solr][:root] = "/opt/solr"
 default[:solr][:version] = "3.3.0"
 default[:solr][:archive_checksum] = "9a3e1d16ccccacd2b55bba73cd76803f6c37c1e8dc5b3b071dc33dc4e443d70b"
 
+default[:solr][:test_core][:enabled] = false
+
 default[:solr][:clustering][:enabled] = true
